1dc1c0c870eaa9b833ec8f7791e4349c03c351db
Dmitry Podgorny <pasis.ua@gmail.com>
2020-09-23 23:36:26 +0300

    ui/ncurses: maintain list of log lines
    This list will be used for redrawing and scrolling features.

--- src/ui_ncurses.c
+++ src/ui_ncurses.c
@@ -26,6 +26,7 @@
 
 #ifdef BUILD_UI_NCURSES
 
+#include "list.h"
 #include "ui.h"
 #include "xmpp.h"
 
@@ -37,23 +38,36 @@
 #include <locale.h>
 #include <readline/history.h>
 #include <readline/readline.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <strophe.h>
 #include <wctype.h>
 
-/*
- * TODO
- *  - Resize
- */
+struct ui_ncurses_line {
+	char *line;
+	size_t chars_nr;
+	uint32_t magic;
+	struct xc_list_link link;
+};
 
 struct xc_ui_ncurses {
 	WINDOW *win_log;
 	WINDOW *win_sep;
 	WINDOW *win_inp;
 	const char *last_status;
+	size_t lines_nr;
+	struct xc_list lines;
+	struct ui_ncurses_line *line_current;
+	bool paged;
 };
 
 #define UI_NCURSES_INPUT_TIMEOUT 1
+#define UI_NCURSES_LINES_MAX 1024
+#define UI_NCURSES_LINE_MAGIC 0xdeadbeef
+
+static struct xc_list_descr ui_ncurses_lines_descr =
+	XC_LIST_DESCR("log lines", struct ui_ncurses_line, link, magic,
+		      UI_NCURSES_LINE_MAGIC);
 
 #define max(a, b)         \
   ({ typeof(a) _a = a;    \
@@ -142,6 +156,23 @@ done:
 	return width;
 }
 
+static size_t ui_ncurses_strwidth(const char *s, size_t offset)
+{
+	return ui_ncurses_strnwidth(s, SIZE_MAX, offset);
+}
+
+static void ui_ncurses_line_destroy_first(struct xc_ui_ncurses *priv)
+{
+	struct ui_ncurses_line *item = xc_list_dequeue(&priv->lines);
+
+	if (item != NULL) {
+		if (priv->line_current == item)
+			priv->line_current = xc_list_head(&priv->lines);
+		free(item->line);
+		free(item);
+	}
+}
+
 static void ui_ncurses_status_set(struct xc_ui_ncurses *priv, const char *status)
 {
 	priv->last_status = status;
@@ -179,11 +210,11 @@ static void ui_ncurses_redisplay_cb(void)
 static void ui_ncurses_redisplay_log(struct xc_ui_ncurses *priv)
 {
 	/*
-	 * TODO Maintain a list of the lines and print required lines.
-	 * This feature will require accurate handling of wrapped lines
-	 * and multibyte characters.
-	 * Scrolling feature will use this function to redraw the window
-	 * after changing position in the list of lines.
+	 * TODO Erase the window. If paged is true, start adding strings
+	 * starting from the line_current. If paged is false, find first line
+	 * from the tail.
+	 * Use line->chars_nr to calculate number of rows taking wrapping into
+	 * account.
 	 */
 	wrefresh(priv->win_log);
 }
@@ -223,7 +254,10 @@ static int ui_ncurses_scrollup_cb(int _x1, int _x2)
 {
 	struct xc_ui_ncurses *priv = g_ui->ui_priv;
 
-	/* TODO Change position in the list of lines. */
+	/*
+	 * TODO Change line_current. If priv->paged is false, start
+	 * counting from the tail.
+	 */
 	ui_ncurses_redisplay_log(priv);
 
 	return 0;
@@ -233,7 +267,10 @@ static int ui_ncurses_scrolldown_cb(int _x1, int _x2)
 {
 	struct xc_ui_ncurses *priv = g_ui->ui_priv;
 
-	/* TODO Change position in the list of lines. */
+	/*
+	 * TODO Change line_current. If we reached bottom, set line_current
+	 * to NULL and set paged to false.
+	 */
 	ui_ncurses_redisplay_log(priv);
 
 	return 0;
@@ -288,6 +325,10 @@ static int ui_ncurses_init(struct xc_ui *ui)
 	scrollok(priv->win_log, TRUE);
 	wtimeout(priv->win_inp, UI_NCURSES_INPUT_TIMEOUT);
 
+	xc_list_init(&priv->lines, &ui_ncurses_lines_descr);
+	priv->lines_nr = 0;
+	priv->line_current = NULL;
+	priv->paged = false;
 	priv->last_status = "";
 	ui->ui_priv = priv;
 
@@ -298,6 +339,10 @@ static void ui_ncurses_fini(struct xc_ui *ui)
 {
 	struct xc_ui_ncurses *priv = ui->ui_priv;
 
+	while (!xc_list_is_empty(&priv->lines)) {
+		ui_ncurses_line_destroy_first(priv);
+	}
+	xc_list_fini(&priv->lines);
 	delwin(priv->win_inp);
 	delwin(priv->win_sep);
 	delwin(priv->win_log);
@@ -365,11 +410,47 @@ static void ui_ncurses_run(struct xc_ui *ui)
 static void ui_ncurses_print(struct xc_ui *ui, const char *msg)
 {
 	struct xc_ui_ncurses *priv = ui->ui_priv;
+	struct ui_ncurses_line *item;
+	const char *s = msg;
+	const char *p;
 
-	waddstr(priv->win_log, msg);
-	waddstr(priv->win_log, "\n");
-	wrefresh(priv->win_log);
-	ui_ncurses_redisplay_cursor(priv);
+	do {
+		if (*s == '\0')
+			break;
+
+		item = malloc(sizeof *item);
+		if (item == NULL)
+			break;
+
+		p = strchr(s, '\n');
+		if (p == NULL) {
+			item->line = strdup(s);
+		} else {
+			item->line = strndup(s, p - s);
+			s = p + 1;
+			while (*s != '\0' && *s == '\r')
+				++s;
+		}
+		if (item->line == NULL) {
+			free(item);
+			break;
+		}
+
+		item->chars_nr = ui_ncurses_strwidth(s, 0);
+		xc_list_insert_tail(&priv->lines, item);
+		if (priv->lines_nr >= UI_NCURSES_LINES_MAX) {
+			ui_ncurses_line_destroy_first(priv);
+		} else {
+			++priv->lines_nr;
+		}
+	} while (p != NULL);
+
+	if (!priv->paged) {
+		waddstr(priv->win_log, msg);
+		waddstr(priv->win_log, "\n");
+		wrefresh(priv->win_log);
+		ui_ncurses_redisplay_cursor(priv);
+	}
 }
 
 static bool ui_ncurses_is_done(struct xc_ui *ui)
